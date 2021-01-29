bc9e782dd1f76954df2dbb840c63ec38aa9fbc22
Alcatraz312 <77152616+Alcatraz312@users.noreply.github.com>
2021-01-29 09:31:48 +0530

    Create Bot2.py

---
+++ Bot2.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+client = commands.Bot(command_prefix="g!")
+@client.event
+async def on_ready():
+    print("Your bot is ready")
+    
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hey there! , I am Guilty Spark , I was created by @Alcatraz b312 using Python")
+@client.command()
+async def Bff(ctx):
+    await ctx.send("My Best friend is Botube")
+@client.command()
+async def creator(ctx):
+    await ctx.send('<@669574365267886130> created me')
+@client.command()
+async def say(ctx,*,arg):
+    await ctx.send(arg)
+
+client.run("Nzk4MDU3NjE4OTI4OTU5NDg4.X_vfEw.xs8zT_3NyJ9C7Xy67hQPKzzNQfQ")
