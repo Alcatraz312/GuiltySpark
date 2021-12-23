abd9cbf7e63be988b1755db720c421d6abb4847d
Alcatraz312 <rathorevyom04@gmail.com>
2021-12-23 11:30:30 +0530

    c40

--- Bot2.py
+++ Bot2.py
@@ -1,4 +1,5 @@
 import discord
+from discord import channel
 from discord.ext import commands , tasks
 import random
 import youtube_dl
@@ -6,6 +7,10 @@ from discord.voice_client import VoiceClient
 import asyncio
 from youtube_dl import YoutubeDL
 import praw
+import os
+import math
+import aiosqlite
+
 client = commands.Bot(command_prefix="g!")
 
 reddit = praw.Reddit(client_id = "BuL6vCcSuCfHGg",
@@ -235,6 +240,114 @@ async def meme(ctx):
 async def server(ctx):
     await ctx.send("https://discord.gg/CwZzF7TAcX")
 
+#level up system
 
+async def initialise():
+    await client.wait_until_ready()
+    client.db = await aiosqlite.connect("expdata.db")
+    await client.db.execute("Create table if not exists guilddata (guild_id int , user_id int , exp int ,Primary Key(guild_id , user_id))")
+
+client.multiplier = 1
+
+@client.event
+async def on_message(message):
+    if not message.author.bot:
+        cursor = await client.db.execute("Insert or ignore into guilddata (guild_id , user_id , exp) Values (? ,? ,?)" , (message.guild.id , message.author.id , 1))
+
+        if cursor.rowcount == 0:
+            await client.db.execute("Update guilddata set exp = exp + 1 where guild_id = ? and user_id = ? " , (message.guild.id , message.author.id))
+            cur = await client.db.execute("Select exp from guilddata where guild_id = ? and user_id = ?", (message.guild.id , message.author,id))
+            data = await cur.fetchone()
+            exp = data[0]
+            lvl = math.sqrt(exp)/client.multiplier
+
+            if lvl.is_integer():
+                await message.channel.send(f"{message.author.mention} great , you are now level {int(lvl)}.")
+        
+        await client.db.commit()
+
+    await client.process_commands(message)
+
+#check stats
+
+@client.command()
+async def stats(ctx , member: discord.member = None):
+    if member is None : member = ctx.author 
+
+    #get user exp
+    async with client.db.execute("Select exp from guilddata where guild_id = ? and user_id = ?" , (ctx.guild.id , member.id)) as cursor:
+        data = await cursor.fetchone()
+        exp = data[0]
+    
+    #calculate rank 
+    async with client.db.execute("select exp from guilddata where guild_id = ?" , (ctx.guild.id,)) as cursor:
+        rank = 1
+        async for value in cursor:
+            if exp < value[0]:
+                rank+=1
+
+    lvl = int(math.sqrt(exp)//client.multiplier)
+
+    current_lvl_exp = (client.multiplier * (lvl))**2
+    next_lvl_exp = (client.multiplier * (lvl+1))**2
+
+    lvl_percentage = ((exp - current_lvl_exp) / (next_lvl_exp - current_lvl_exp)) * 100
+
+    emb = discord.Embed(title = f"Status for {member.name}" , colour = discord.colour.red())
+    emb.add_field(name = "Level" , value = str(lvl))
+    emb.add_field(name = "Exp" , value = f"{exp}/{next_lvl_exp}")
+    emb.add_field(name = "Rank" , value = f"{rank}/{ctx.guild.member_count}")
+    emb.add_field(name  = "Level Progress" , value = f"{round(lvl_percentage , 2)}%")
+
+    
+    #leader board 
+    @client.command()
+    async def leaderboard(ctx):
+        buttons = {}
+        for i in range (1,6):
+            buttons[f"{i}\N{COMBINING ENCLOSING KEYCAP}"] = i
+        
+        previous_page = 0
+        current = 1
+        index = 1
+        entries_per_page = 10
+
+        emb = discord.Embed(title = f"Leaderboard page {current} " , description = "" , colour = discord.colour.red())
+        msg = await ctx.send(emb =emb)
+
+        for button in buttons:
+            await msg.add_reaction(button)
+
+        while True:
+            if current != previous_page:
+                emb.title = f"Leaderboard page {current}"
+                emb.description = ""
+
+                async with client.db.execute("select user_id , exp from guilddata where guild_id = ? order by exp desc limit ? offset ?" , (ctx.guild.id , entries_per_page , entries_per_page*(current - 1) , )) as cursor:
+                    index = entries_per_page * (current - 1)
+
+                    async for entry in cursor:
+                        index+=1
+                        member_id , exp = entry
+                        member = ctx.guild.get_member(member_id)
+                        emb.description += f"{index}) {member.mention} : {exp}\n"
+
+                    await msg.edit(emb = emb)
+            
+            try:
+                reaction , user = await client.wait_for("reaction add" , check = lambda reaction , user: user == ctx.author and reaction.emoji in buttons , timeout = 60.0)
+
+            except asyncio.TimeoutError:
+                return await msg.clear_reactions()
+            
+            else:
+                previous_page = current
+                await msg.remove_reaction(reaction.emoji , ctx.author)
+                current = buttons[reaction.emoji]
+
+
+client.loop.create_task(initialise())
 client.run("Nzk4MDU3NjE4OTI4OTU5NDg4.X_vfEw.xs8zT_3NyJ9C7Xy67hQPKzzNQfQ")
+
+asyncio.run(client.db.close())
 
